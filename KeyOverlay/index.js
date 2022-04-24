379bd45a9bbfabac75a5f4d05826f8c42f558317
robloxxa <robloxxa@yandex.ru>
2022-04-24 19:44:40 +0300

    Added KeyOverlay

---
+++ KeyOverlay/index.js
@@ -0,0 +1,38 @@
+const socket = new ReconnectingWebSocket("ws://" + "localhost:24050" + "/ws");
+const keys = document.getElementById('keys')
+let k1 = new KeyOverlay('k1', 'k1Tiles', {speed: 0.4, keyTextId:"k1Text"}),
+    k2 = new KeyOverlay('k2', 'k2Tiles', {speed: 0.4}),
+    m1 = new KeyOverlay('m1', 'm1Tiles', {speed: 0.4}),
+    m2 = new KeyOverlay('m2', 'm2Tiles', {speed: 0.4}),
+    tempState;
+socket.onopen = () => {
+    console.log("Successfully Connected");
+};
+
+socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!");
+};
+
+socket.onerror = error => {
+    console.error(error);
+};
+
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+
+    if (data.menu.state !== tempState) {
+        tempState = data.menu.state
+        if (tempState !== 2) {
+            keys.style.opacity = '0'
+        } else {
+            keys.style.opacity = '1'
+        }
+    }
+    k1.update(data.gameplay.keyOverlay.k1)
+    k2.update(data.gameplay.keyOverlay.k2)
+    m1.update(data.gameplay.keyOverlay.m1)
+    m2.update(data.gameplay.keyOverlay.m2)
+}
+
